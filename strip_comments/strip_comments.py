f9e0bab332327d6299a254b7e93938841654ba55
Anna Fedyna <annafedyna03@gmail.com>
2023-12-11 14:58:24 +0000

    first attemp

--- strip_comments/strip_comments.py
+++ strip_comments/strip_comments.py
@@ -1,2 +1,9 @@
+import re
+
 def strip_comments(strng, markers):
-    pass
+    for marker in markers:
+        strng = re.sub(rf'{re.escape(marker)}.*', '', strng)
+    return re.sub(r'\s*$', '', strng, flags=re.MULTILINE)
+
+result = strip_comments('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!'])
+print(result)
