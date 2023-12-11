14e3d5915c148696eb8eb45468dc4db737ec1424
Anna Fedyna <annafedyna03@gmail.com>
2023-12-11 12:55:35 +0000

    train.py

--- train.py
+++ train.py
@@ -1,11 +1,2 @@
-from functools import reduce
-from operator import add
-
-def order_weight(strng):
-    a = strng.strip().split(' ')
-
-    res = sorted(a, key = lambda num: (reduce(add, map(int, num)), num))
-    
-    return ' '.join(res)
-
-print(order_weight('2000 10003 1234000 14444 9999 11 11 22 123'))
+def strip_comments(strng, markers):
+    pass
