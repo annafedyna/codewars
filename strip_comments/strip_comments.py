69ef238a18cd842e3464319ad6fefc77b8452304
Anna Fedyna <annafedyna03@gmail.com>
2023-12-11 16:28:57 +0000

    add better solution

--- strip_comments/strip_comments.py
+++ strip_comments/strip_comments.py
@@ -15,5 +15,10 @@ def strip_comments(strng, markers):
         
     return '\n'.join(arr)
 
-result = strip_comments('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!'])
-print(result) 
+
+# BETTER PRACTICE 
+def solution(string,markers):
+    s_list = string.split('\n')
+    for marker in markers:
+        s_list = [item.split(marker)[0].strip() for item in s_list]
+    return '\n'.join(s_list)
