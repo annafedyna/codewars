2c8bd665bb3d2460bf04f6329f0411901ba7792a
Anna Fedyna <annafedyna03@gmail.com>
2023-12-12 12:43:16 +0000

    gridy algorithm O(n)

--- 5kyu/max_subarray_sum.py
+++ 5kyu/max_subarray_sum.py
@@ -2,16 +2,15 @@ from collections import deque
 
 def max_sequence(arr):
     arr = deque(arr)
-    max = 0
-    for i in range(len(arr)):
-        sub_sum = 0
-        for j in range(len(arr)):
-            sub_sum = sub_sum + arr[j]
-            if max < sub_sum:
-                max = sub_sum
-        
-        arr.popleft()
-    return max
+    max_sum = 0
+    sub_sum = 0
+    while arr:
+        sub_sum += arr.popleft()
+        if sub_sum < 0:
+            sub_sum = 0  
+        if sub_sum > max_sum :
+            max_sum = sub_sum   
+    return max_sum
         
 
 print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
