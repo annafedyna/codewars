7c2d8ce965cf4228cdbeefbe92dd29b546c999a2
Anna Fedyna <annafedyna03@gmail.com>
2023-12-12 22:59:15 +0000

    add solution2

--- 5kyu/string_increment/string_increment.py
+++ 5kyu/string_increment/string_increment.py
@@ -1,5 +1,5 @@
 import re
-
+# Solution 1 
 def increment_string(strng):
     
     num = re.findall('[0-9]+$', strng)
@@ -9,5 +9,13 @@ def increment_string(strng):
         return re.sub('[0-9]+$', str(int(num) + 1).zfill(len(num)), strng)
     else :
         return strng + str(1)
+   
+ # Solution 2  
+ 
+def increment_string(strng):
+    head = strng.rstrip('0123456789')
+    tail = strng[len(head):]
+    if tail == "": return strng+"1"
+    return head + str(int(tail) + 1).zfill(len(tail))
 
-print(increment_string('foobar0099'))
+print(increment_string('foo7bar000099'))
