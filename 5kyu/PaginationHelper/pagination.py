da8b9149061c7b52b4d7ef53b7859a7f7dac6b5f
Anna Fedyna <annafedyna03@gmail.com>
2023-12-13 11:50:21 +0000

    pagination solution 2

--- 5kyu/PaginationHelper/pagination.py
+++ 5kyu/PaginationHelper/pagination.py
@@ -36,3 +36,25 @@ class PaginationHelper:
     
 helper = PaginationHelper(['a','b','c','d','e','f', 'j', 'h', 'g'], 4)
 print(helper.page_index(5))
+
+
+# Solution 2
+
+class PaginationHelper:
+    def __init__(self, collection, items_per_page):
+        self._item_count = len(collection)
+        self.items_per_page = items_per_page
+
+    def item_count(self):
+        return self._item_count
+
+    def page_count(self):
+        return -(self._item_count // -self.items_per_page)
+
+    def page_item_count(self, page_index):
+        return min(self.items_per_page, self._item_count - page_index * self.items_per_page) \
+            if 0 <= page_index < self.page_count() else -1
+
+    def page_index(self, item_index):
+        return item_index // self.items_per_page \
+            if 0 <= item_index < self._item_count else -1
