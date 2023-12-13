0908600b648f0e1e4de13d4ade5b238bfa771a9f
Anna Fedyna <annafedyna03@gmail.com>
2023-12-13 16:17:26 +0000

    nesting compare + best practice

--- 4kyu/Nesting Structure Comparison/nesting_compare.py
+++ 4kyu/Nesting Structure Comparison/nesting_compare.py
@@ -6,19 +6,23 @@ def same_structure_as(original, other):
         return False
 
     for i in range(len(original)):
-        if type(original[i]) == type(other[i]):
-            if type(original[i]) == list:
-                if not same_structure_as(original[i], other[i]):
-                    return False
-        else:
+        if type(original[i]) == type(other[i]) == list:
+            return same_structure_as(original[i], other[i])
+        elif bool(type(original[i])== list )!= bool(type(other[i])==list):
             return False
 
     return True
 
 
-print(same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ]))
+print(same_structure_as([1,'[',']'],['[',']',1]))
 
 print(same_structure_as([ [ [], [] ] ],   [ [ [], [] ] ] ))
 
+# BEST PRACTICE
 
-# This code fails:  [1,'[',']'] same as ['[',']',1]: False should equal True
+def same_structure(original, other):
+    return type(original) == type(other) and (len(original) == len(other) and all(map(same_structure,original, other))) if type(original) == list else 1
+
+print(same_structure([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ]))
+
+print(same_structure([ [ [], [] ] ],   [ [ [], [] ] ] ))
