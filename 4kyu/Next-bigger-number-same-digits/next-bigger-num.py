1a7898577239260b54d76b45bc97dc37b3f23cf0
Anna Fedyna <annafedyna03@gmail.com>
2023-12-21 17:32:23 +0000

    next-bigger-better-practice

--- 4kyu/Next-bigger-number-same-digits/next-bigger-num.py
+++ 4kyu/Next-bigger-number-same-digits/next-bigger-num.py
@@ -1,25 +1,16 @@
+
+
 def next_bigger(n):
     n_list = list(str(n))
-    fs = None
     
-    for i in range(len(n_list) - 1, 0, -1):
-        if n_list[i] > n_list[i - 1]:   
-            fs = n_list[i -1]
-            index = i-1
-            break
+    for i in range(len(n_list)-1 , 0, -1):
+        if n_list[i] > n_list[i-1]:
+            tail = n_list[i-1:]
+            min_el = min(filter(lambda x: x > tail[0],tail))
+            tail.remove(min_el)
+            tail.sort()
+            return int(''.join(n_list[:i-1] + [min_el] + tail))
             
-    if not fs:
-        return -1
-
-    right = sorted([n_list[j] for j in range(len(n_list)-1,index,-1)])
-    fss  = fs
-    for e in range(len(right)):
-        if right[e] > fs:
-            fs = right[e]
-            right[e] = fss
-            break
-    
-    res = [n_list[i] for i in range(index)] + [fs] + right
-    return int(''.join(res))
+    return -1
 
 print(next_bigger(534976))
