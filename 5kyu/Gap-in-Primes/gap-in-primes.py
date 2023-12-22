181d7548571f3c75205f2060c866fb54c675c983
Anna Fedyna <annafedyna03@gmail.com>
2023-12-22 16:05:43 +0000

    correct but time out

--- 5kyu/Gap-in-Primes/gap-in-primes.py
+++ 5kyu/Gap-in-Primes/gap-in-primes.py
@@ -7,11 +7,13 @@ def isPrime(x):
 
 
 def gap(g, m, n):
-    res = []
-    for num in range(m, n+1-g):
-        if isPrime(num) and isPrime(num+g):
-            res += (num, num+g)
-    return res if res != [] else None
+    primes_num = []
+    for num in range(m, n+1):
+        if isPrime(num): primes_num.append(num)
+        
+    for i in range(len(primes_num)-1):
+        if abs(primes_num[i] - primes_num[i+1]) == g:
+            return [primes_num[i], primes_num[i+1]]
 
 
 print(gap(4,100,110))
