432ea705cfd6b9742c9e6af1c57ebece468ff52c
Anna Fedyna <annafedyna03@gmail.com>
2023-12-24 23:39:52 +0000

    observed-pin first code

--- 4kyu/The-observed-Pin/the-observed-pin.py
+++ 4kyu/The-observed-Pin/the-observed-pin.py
@@ -1,6 +1,31 @@
+from itertools import product    
+
+def combinations(*arrays):
+    return list(product(*arrays))
+
 def get_pins(observed):
-    pass 
+    d = [
+        ['1','4','2'],
+        ['2', '1', '3', '5'],
+        ['3','2','6'],
+        ['4','1','7','5'],
+        ['5','2', '4','8', '6'],
+        ['6', '3', '5', '9'],
+        ['7','4', '8'],
+        ['8', '5', '7','9','0'],
+        ['9','6', '8'],
+        ['0','8']
+    ]
+    
+    arrays = []
+    for i in range(len(observed)):
+        for j in range(len(d)):
+            if observed[i] == d[j][0]:
+                arrays.append(d[j])
+    
+    res = combinations(*arrays)
+
+    return [''.join(a) for a in res]
 
 
-
-print(get_pins('11'))
+print(get_pins('36'))
