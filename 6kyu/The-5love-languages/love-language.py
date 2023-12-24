26ecaeb900e103fd91a793b97a8e46322b135f9e
Anna Fedyna <annafedyna03@gmail.com>
2023-12-24 17:01:04 +0000

    love-language implementation

--- 6kyu/The-5love-languages/love-language.py
+++ 6kyu/The-5love-languages/love-language.py
@@ -1,5 +1,32 @@
-# from preloaded import LOVE_LANGUAGES
+import random
 
 def love_language(partner, weeks):
-    # your lovely code here
-    return "words"
+        love_lang=["words", "acts", "gifts", "time", "touch"]
+        positive_response = [0]*5
+        neutral_response = [0]*5 # unnecessary
+        for i in range(7*weeks):
+            j = i%5
+            if partner.response(love_lang[j]) == 'positive':
+                positive_response[j] += 1
+            else:
+                neutral_response[j] += 1
+        return love_lang[positive_response.index(max(positive_response))]
+
+class TestPartner:
+    def __init__(self, main_lang):
+        self.main = main_lang
+    def response(self, language):
+        r = random.random()
+        if language == self.main:
+            if r < 0.85: return 'positive'
+            else:        return 'neutral'
+        else: # language != self.main
+            if r < 0.15: return 'positive'
+            else:        return 'neutral'
+
+weeks = 6
+partner = TestPartner('words')
+print(love_language(partner, weeks), 'words')
+
+partner = TestPartner('gifts')
+print(love_language(partner, weeks), 'gifts')
